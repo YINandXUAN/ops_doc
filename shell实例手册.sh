3dbbf57190a0e1650cec5d1cae51753e380d95ad
YINandXUAN <659030105@qq.com>
2015-06-11 15:16:54 +0800

    Update shell实例手册.sh

--- shell实例手册.sh
+++ shell实例手册.sh
@@ -11,7 +11,6 @@
     请使用"notepad++"或其它编辑器打开此文档, "alt+0"将函数折叠后方便查阅    
     请勿删除信息, 转载请说明出处, 抵制不道德行为    
     错误在所难免, 还望指正！    
-        
     [python实例手册] [shell实例手册] [LazyManage运维批量管理(shell/python两个版本)]
     网盘更新下载地址:    http://pan.baidu.com/s/1sjsFrmX
     github更新下载地址:  https://github.com/liquanzhou/ops_doc
